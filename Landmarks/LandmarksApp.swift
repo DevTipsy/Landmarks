475b1c569e7c39dd542009cf3a5972e485ddcb03
Thibault Sanclemente <84449794+DevTipsy@users.noreply.github.com>
2023-03-21 17:55:39 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by DevTipsy94 on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
